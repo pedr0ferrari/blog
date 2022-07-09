d33171dbaf0228898c4171b843d23c286a480fa6
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2022-07-09 15:34:06 -0300

    create first input using state

--- pages/contato.tsx
+++ pages/contato.tsx
@@ -1,11 +1,29 @@
+import { Flex, Input, FormLabel } from "@chakra-ui/react";
+import { useState } from "react";
 import Header from "../components/header/header";
 import Main from "../layouts/Main";
 
 const Contato = () => {
+  const [nome, setNome] = useState("");
+  console.log(nome);
   return (
     <>
       <Header />
-      <Main>Flea corporation</Main>
+      <Main>
+        <Flex as="form" direction="column">
+          <FormLabel>Nome</FormLabel>
+          <Input
+            placeholder="Nome completo"
+            value={nome}
+            onChange={(event) => {
+              setNome(event.target.value);
+            }}
+          />
+          <FormLabel>Email</FormLabel>
+          <Input placeholder="Email" />
+        </Flex>
+        Flea corporation
+      </Main>
     </>
   );
 };
